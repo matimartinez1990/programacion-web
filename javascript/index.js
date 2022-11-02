e152c0d1bcedfe1e026c6809aa2b4f059708651e
Matias Daniel Martinez <matiasmartinez0721@gmail.com>
2022-11-01 21:08:05 -0300

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+let nombre="Matias";
+let apellido="Martinez";
+const edad=31;
+let mascota="Zeus";
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(nombre + " " + apellido);
+
+
+let entrada=prompt("ingrese su nombre");
+let salida=(entrada + " " + "ingresado");
+alert(salida)
+console.log(salida)
